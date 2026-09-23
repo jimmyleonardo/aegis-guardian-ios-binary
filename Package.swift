cb7114ddf30ac2387f19a7bf1156396df8edfd8f
Jimmy Leonardo <jimm.leonardo@gmail.com>
2026-09-23 15:03:28 +0700

    feat: publish Swift Package Manager binary 2.1.2

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AegisGuardian",
-            url: "https://github.com/jimmyleonardo/aegis-guardian-ios-binary/releases/download/ios-2.1.1/AegisGuardian.xcframework.zip",
-            checksum: "3e7ed0fc4d3cf0c5a5e3a7ee0e285f95655ffe005c859cde898d757f634e4576"
+            url: "https://github.com/jimmyleonardo/aegis-guardian-ios-binary/releases/download/ios-2.1.2/AegisGuardian.xcframework.zip",
+            checksum: "fb8c52c9727e43d60969bbed9ea4643e03eba25b834c5ce8b08f7205ced107e1"
         )
     ]
 )
--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Public XCFramework assets for AegisGuardian iOS releases.
 ## CocoaPods
 
 ```ruby
-pod 'AegisGuardian', '2.1.1'
+pod 'AegisGuardian', '2.1.2'
 ```
 
 ## Swift Package Manager
 
 ```swift
-.package(url: "https://github.com/jimmyleonardo/aegis-guardian-ios-binary.git", exact: "2.1.1")
+.package(url: "https://github.com/jimmyleonardo/aegis-guardian-ios-binary.git", exact: "2.1.2")
 ```
 
 Link the `AegisGuardian` product and `import AegisGuardian`. Requires iOS 15+ and Xcode 26+.
